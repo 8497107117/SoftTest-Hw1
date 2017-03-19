a9d0948fbb5da5a375c41a502b48d34d4960826e
Taku <gcobs135102@gmail.com>
2017-03-20 00:28:24 +0900

    Robust Boundary Value Test Pass

--- Triangle/triangle.cpp
+++ Triangle/triangle.cpp
@@ -3,6 +3,13 @@
 string isTriangle(int a, int b, int c) {
     string s;
 
+    if(a < 1 || a > 200)
+        return "Value of a is not in the range of permitted values";
+    else if(b < 1 || b > 200)
+        return "Value of b is not in the range of permitted values";
+    else if(c < 1 || c > 200)
+        return "Value of c is not in the range of permitted values";
+
     if(a + b <= c || a + c <= b || b + c <= a)
         s = "Not a triangle";
     else if(a == b && b == c)
