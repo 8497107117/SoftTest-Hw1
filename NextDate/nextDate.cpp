4d73e690ff11fa942a3899b6c868da1b4c32d3ea
Taku <gcobs135102@gmail.com>
2017-03-21 00:43:33 +0900

    Normal Boundary Value Test Pass

--- NextDate/nextDate.cpp
+++ NextDate/nextDate.cpp
@@ -1,9 +1,77 @@
 #include "nextDate.h"
 
 bool leapyear(int y) {
-    return false;
+    return y%4 == 0 ? y%100 == 0 ? y%400 == 0 ? true : false : true : false ;
 }
 
 string nextDate(int d, int m, int y) {
-    return "";
+    string s = "";
+    bool rangeD = true, rangeM = true, rangeY = true;
+    int nd, nm, ny;
+
+    if(y < 1812 || y > 2012)
+        rangeY = false;
+    if(m < 1 || m > 12)
+        rangeM = false;
+    if(!rangeY)
+        return "year not in range: 1812~2012";
+    if(!rangeM)
+        return "month not in range: 1~12";
+
+    if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12 ) {
+        if(d < 1 || d > 31)
+            rangeD = false;
+        if(!rangeD)
+            return "day not in range: 1~31";
+
+        if(d == 31) {
+            nd = 1;
+            nm = m + 1;
+            ny = y;
+        }
+        else {
+            nd = d + 1;
+            nm = m;
+            ny = y;
+        }
+    }
+    else if(m != 2) {
+        if(d < 1 || d > 30)
+            rangeD = false;
+        if(!rangeD)
+            return "day not in range: 1~30";
+
+        if(d == 30) {
+            nd = 1;
+            nm = m + 1;
+            ny = y;
+        }
+        else {
+            nd = d + 1;
+            nm = m;
+            ny = y;
+        }
+    }
+    else {
+        bool leap = leapyear(y);
+        if(d < 1 || (d > 28 && !leap) || (d > 29 && leap))
+            rangeD = false;
+        if(!rangeD)
+            return "day not in range: 1~28or29";
+
+        if((d == 28 && !leap) || (d == 29 && leap)) {
+            nd = 1;
+            nm = m + 1;
+            ny = y;
+        }
+        else {
+            nd = d + 1;
+            nm = m;
+            ny = y;
+        }
+    }
+    
+    s = to_string(nd) + "," + to_string(nm) + "," + to_string(ny);
+
+    return s;
 }
